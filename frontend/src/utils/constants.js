352419904b6f91cbef0bc01ef51aa5f8d6874a58
Kirill Kamratov <kirill.kamratov@yandex.ru>
2023-07-03 00:18:05 +0300

    pre-docker

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = 'https://api.kamratov.students.nomoreparties.sbs';
+export const URL = 'https://api.kpdkamratov.students.nomoreparties.sbs';
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
