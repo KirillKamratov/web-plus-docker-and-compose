04e0fd6cf46f92c80f3cfa80ed27b3f5636fb9c9
Kirill Kamratov <kirill.kamratov@yandex.ru>
2023-07-02 20:08:41 +0300

    pre-docker

--- backend/src/utils/jwt/jwt.strategy.ts
+++ backend/src/utils/jwt/jwt.strategy.ts
@@ -3,7 +3,7 @@ import { Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { UsersService } from '../../users/users.service';
+import {UsersService} from "../../users/users.service";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -13,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: configService.get<string>('jwt_secret'),
+      secretOrKey: configService.get<string>('JWT_SECRET'),
       ignoreExpiration: false,
     });
   }
